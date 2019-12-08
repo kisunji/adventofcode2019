6c6049dc1f36648e5300eec6eb1a3c392c96db01
Chris Sungjin Kim <kisunji92@gmail.com>
2019-12-08 18:07:12 -0500

    Complete day 7 part 1

---
+++ day7/permutation.go
@@ -0,0 +1,21 @@
+package main
+
+// Functions copied from https://stackoverflow.com/a/30230552
+
+func nextPerm(p []int) {
+	for i := len(p) - 1; i >= 0; i-- {
+		if i == 0 || p[i] < len(p)-i-1 {
+			p[i]++
+			return
+		}
+		p[i] = 0
+	}
+}
+
+func getPerm(orig, p []int) []int {
+	result := append([]int{}, orig...)
+	for i, v := range p {
+		result[i], result[i+v] = result[i+v], result[i]
+	}
+	return result
+}
